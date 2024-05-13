0054fcb3c6e18a8e824d379a9b4740159f1c4f64
DinnaWitness <witnessdinna@gmail.com>
2024-05-13 16:24:47 +1000

    Flask created

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,32 +1,84 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
 
 #################################################
 # Database Setup
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # Declare a Base using `automap_base()`
-
+Base = automap_base()
 # Use the Base class to reflect the database tables
-
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+# Homepage and list of available routes
+@app.route("/")
+def home():
+    """List all available routes."""
+    return (
+        f"Welcome to the Climate App API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the JSON representation of the dictionary."""
+
+# precipitation route
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a JSON list of stations from the dataset."""
+   
+# stations route
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a JSON list of temperature observations for the previous year."""
+   
+# tobs route
+
+@app.route("/api/v1.0/<start>")
+def temp_stats_start(start):
+    """Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature
+    for a specified start date."""
+    
+# start date route
+
+@app.route("/api/v1.0/<start>/<end>")
+def temp_stats_range(start, end):
+    """Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature
+    for a specified start-end range."""
+   
+# start-end range route
+
+if __name__ == "__main__":
+    app.run(debug=True)
